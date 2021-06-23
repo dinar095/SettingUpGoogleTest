f7140971bbdec34694df5917a895bbb170a1ff84
Dinar <dinar95@mail.ru>
2021-06-23 19:45:55 +0300

    Test files

---
+++ cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/dinar/CLionProjects/SettingUpGoogleTest/lib/googletest-master/googletest
+# Build directory: /mnt/c/Users/dinar/CLionProjects/SettingUpGoogleTest/cmake-build-debug/lib/googletest-master/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
