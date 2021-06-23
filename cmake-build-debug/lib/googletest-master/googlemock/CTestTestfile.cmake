f14b2ede4de4920a75a51d768457620f33cf8e94
dinar095 <47459993+dinar095@users.noreply.github.com>
2021-06-23 19:49:43 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/dinar/CLionProjects/SettingUpGoogleTest/lib/googletest-master/googlemock
-# Build directory: /mnt/c/Users/dinar/CLionProjects/SettingUpGoogleTest/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
